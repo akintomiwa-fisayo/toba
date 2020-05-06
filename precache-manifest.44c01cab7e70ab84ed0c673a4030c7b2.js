8e7fc7b4a8111b7e14f4e8bef2c63e5b76d1f8de
akintomiwa fisayo <akintomiwa.fisayo@gmail.com>
2020-05-06 18:13:51 +0100

    Updates

---
+++ precache-manifest.44c01cab7e70ab84ed0c673a4030c7b2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c108910d3866ef1fed0b0ebae25541",
+    "url": "/toba/index.html"
+  },
+  {
+    "revision": "fa30b82d78605ce5ebcc",
+    "url": "/toba/static/css/main.5bcd5c77.chunk.css"
+  },
+  {
+    "revision": "9875eb6dd9e6265c45c3",
+    "url": "/toba/static/js/2.2993495e.chunk.js"
+  },
+  {
+    "revision": "fa30b82d78605ce5ebcc",
+    "url": "/toba/static/js/main.7f8a1afe.chunk.js"
+  },
+  {
+    "revision": "f74015fab02db484be08",
+    "url": "/toba/static/js/runtime-main.fda5d187.js"
+  },
+  {
+    "revision": "47f711bd1524b9283aa92d3d74db16a8",
+    "url": "/toba/static/media/BebasNeue.47f711bd.ttf"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/toba/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "f458c03ef7bc399e504527243767cfe0",
+    "url": "/toba/static/media/backPath.f458c03e.svg"
+  },
+  {
+    "revision": "f181e73b2cadb0f0ac915af0f6db1809",
+    "url": "/toba/static/media/banner.f181e73b.jpg"
+  },
+  {
+    "revision": "f1a4a058fbba1e35a406188ae7eddaf8",
+    "url": "/toba/static/media/header_light.f1a4a058.woff2"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/toba/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "0f6440a6e1ea5047d0f9220581a494d6",
+    "url": "/toba/static/media/mic.0f6440a6.jpg"
+  }
+]);
